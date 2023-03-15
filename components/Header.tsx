ef06d929dc65cc0a7d6e2e9d7631c628560720a7
lmnnarciso <lmnnarciso14@gmail.com>
2023-03-15 17:39:21 +0800

    Fix fonts

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,41 +18,41 @@ export const Header = () => {
 
   useEffect(() => {
     const handleResize = () => setIsMenuOpen(false);
-    window.addEventListener("orientationchange", handleResize)
-    window.addEventListener("resize", handleResize)
+    window.addEventListener("orientationchange", handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener("orientationchange", handleResize)
-      window.removeEventListener("resize", handleResize)
-    }
-  }, [setIsMenuOpen])
+      window.removeEventListener("orientationchange", handleResize);
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [setIsMenuOpen]);
 
   return (
     <header className="fixed top-0 left-0 w-full backdrop-blur-[12px]">
-      <Container className="flex h-[var(--navigation-height)] border-b border-white-a08">
+      <Container className="flex h-[var(--navigation-height)] border-b border-transparent-white">
         <Link className="flex items-center text-md" href="/">
-          <Logo className="w-[1.8rem] h-[1.8rem] mr-4" />
+          <Logo className="mr-4 h-[1.8rem] w-[1.8rem]" />
           Linear
         </Link>
         <div
           className={classNames(
             "transition-[visibility] md:visible",
-            isMenuOpen ? "visible " : "invisible delay-500"
+            isMenuOpen ? "visible " : "delay-500 invisible"
           )}
         >
           <nav
             className={classNames(
-              "md:transition-none md:opacity-100 transition-opacity duration-500 fixed md:relative top-navigation-height md:top-0 left-0 md:h-auto md:block w-full md:w-auto h-[calc(100vh_-_var(--navigation-height))] bg-background md:bg-transparent overflow-auto md:overflow-hidden md:translate-x-0",
+              "fixed top-navigation-height left-0 h-[calc(100vh_-_var(--navigation-height))] w-full overflow-auto bg-background transition-opacity duration-500 md:relative md:top-0 md:block md:h-auto md:w-auto md:translate-x-0 md:overflow-hidden md:bg-transparent md:opacity-100 md:transition-none",
               isMenuOpen
-                ? "opacity-100 translate-x-0"
-                : "opacity-0 translate-x-[-100vw]"
+                ? "translate-x-0 opacity-100"
+                : "translate-x-[-100vw] opacity-0"
             )}
           >
             <ul
               className={classNames(
-                "flex h-full  flex-col md:flex-row items-center [&_li]:h-top-navigation-height",
-                "[&_li]:w-full [&_li]:border-b [&_li]:md:border-b-0 [&_li]:border-gray-border px-6 md:[&_li]:ml-6",
-                "[&_a]:duration-300 [&_a]:translate-y-8 md:[&_a]:translate-y-0 [&_a]:transition-[colors,transform] [&_a]:md:transition-colors [&_a]:flex [&_a]:items-center [&_a]:w-full [&_a]:h-navigation-height [&_a]:text-md md:[&_a)]:text-sm [&_a:hover]:text-gray ",
+                "[&_li]:h-top-navigation-height flex  h-full flex-col items-center md:flex-row",
+                "[&_li]:border-grey-border px-6 [&_li]:w-full [&_li]:border-b md:[&_li]:ml-6 [&_li]:md:border-b-0",
+                "md:[&_a)]:text-md [&_a:hover]:text-grey [&_a]:flex [&_a]:h-navigation-height [&_a]:w-full [&_a]:translate-y-8 [&_a]:items-center [&_a]:text-lg [&_a]:transition-[colors,transform] [&_a]:duration-300 md:[&_a]:translate-y-0 [&_a]:md:transition-colors ",
                 isMenuOpen ? "[&_a]:translate-y-[0]" : ""
               )}
             >
@@ -80,8 +80,8 @@ export const Header = () => {
             </ul>
           </nav>
         </div>
-        <div className="ml-auto h-full flex items-center">
-          <Link href="#" className="text-sm mr-6">
+        <div className="ml-auto flex h-full items-center">
+          <Link href="#" className="mr-6 text-sm">
             Login
           </Link>
           <Button href="#">Sign up</Button>
